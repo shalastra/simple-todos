59132b9db71e95b6532a00a3c04e0d0068c98240
szymonhalastra <szymon.halastra@gmail.com>
2016-06-11 10:25:04 +0200

    Input field added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -6,6 +7,18 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+	handleSubmit(event) {
+		event.preventDefault();
+
+		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+		Tasks.insert({
+			text,
+			createdAt: new Date()
+		});
+
+		ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
 
 	renderTasks() {
 		return this.props.tasks.map((task) => (
@@ -18,6 +31,10 @@ class App extends Component {
 			<div className="container">
 				<header>
 					<h1>Todo List</h1>
+
+					<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+						<input type="text" ref="textInput" placeholder="Type to add new tasks" />
+					</form>
 				</header>
 
 				<ul>
@@ -34,6 +51,6 @@ App.propTypes = {
 
 export default createContainer(() => {
 	return {
-		tasks: Tasks.find({}).fetch(),
+		tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
 	};
 }, App);
